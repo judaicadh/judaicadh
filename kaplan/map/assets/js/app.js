79c30efe6ac52b7351b06027513a6e167a69ac24
Laura Newman Eckstein <laura.n.eckstein@gmail.com>
2017-09-26 09:20:22 -0400

    Ed

--- kaplan/map/assets/js/app.js
+++ kaplan/map/assets/js/app.js
@@ -426,7 +426,7 @@ Watercolor: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z
  
 Satellite : L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-});
+})};
 
 
 map = L.map("map", {
